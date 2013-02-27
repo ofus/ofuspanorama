31da3888135e6de828723ba7b9c708699c3ce2ed
Andrew Joseph <andrew@aljweb.com>
2013-02-27 01:42:45 -0600

    change base to list of panoramas

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Ofus stuff</title>
+    <meta name="description" content="Ofus's stuff" />
+</head>
+<body>
+<div>
+    <ul>
+        <li><a href="sprint.html" title="Sprint">Sprint</a></li>
+        <li><a href="snow.html" title="Snow">Snow</a></li>
+        <li><a href="snow2.html" title="snow2">Snow2</a></li>
+    </ul>
+</div>
+</body>
+</html>
